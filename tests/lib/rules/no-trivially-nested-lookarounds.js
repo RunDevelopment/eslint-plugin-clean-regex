a66c7b15d7fc95a9dce374c7f2f400060537cdc6
Michael Schmidt <mitchi5000.ms@googlemail.com>
2019-06-16 17:43:40 +0200

    New rule against trivially nested lookarounds

---
+++ tests/lib/rules/no-trivially-nested-lookarounds.js
@@ -0,0 +1,58 @@
+/**
+ * @fileoverview Disallow trivially nested lookarounds
+ * @author Michael Schmidt
+ */
+
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/no-trivially-nested-lookaround");
+const { RuleTester } = require("eslint");
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2018 } });
+
+const errors = [{ message: /^Trivially nested lookaround:/ }];
+
+ruleTester.run("no-trivially-nested-lookaround", rule, {
+	valid: [
+		String(/(?=(?=a)b)/),
+
+		// these anchors cannot be negated, so they have to be allowed
+		String(/(?!$)/),
+		String(/(?<!$)/),
+		String(/(?!^)/),
+		String(/(?<!^)/),
+	],
+	invalid: [
+		{ code: String(/(?=$)/), output: String(/$/), errors },
+		{ code: String(/(?=^)/), output: String(/^/), errors },
+		{ code: String(/(?<=$)/), output: String(/$/), errors },
+		{ code: String(/(?<=^)/), output: String(/^/), errors },
+		{ code: String(/(?=\b)/), output: String(/\b/), errors },
+		{ code: String(/(?!\b)/), output: String(/\B/), errors },
+		{ code: String(/(?<=\b)/), output: String(/\b/), errors },
+		{ code: String(/(?<!\b)/), output: String(/\B/), errors },
+
+		// all trivially nested lookarounds can be written as one lookaround
+		// Note: The inner lookaround has to be negated if the outer one is negative.
+		{ code: String(/(?=(?=a))/), output: String(/(?=a)/), errors },
+		{ code: String(/(?=(?!a))/), output: String(/(?!a)/), errors },
+		{ code: String(/(?=(?<=a))/), output: String(/(?<=a)/), errors },
+		{ code: String(/(?=(?<!a))/), output: String(/(?<!a)/), errors },
+		{ code: String(/(?!(?=a))/), output: String(/(?!a)/), errors },
+		{ code: String(/(?!(?!a))/), output: String(/(?=a)/), errors },
+		{ code: String(/(?!(?<=a))/), output: String(/(?<!a)/), errors },
+		{ code: String(/(?!(?<!a))/), output: String(/(?<=a)/), errors },
+		{ code: String(/(?<=(?=a))/), output: String(/(?=a)/), errors },
+		{ code: String(/(?<=(?!a))/), output: String(/(?!a)/), errors },
+		{ code: String(/(?<=(?<=a))/), output: String(/(?<=a)/), errors },
+		{ code: String(/(?<=(?<!a))/), output: String(/(?<!a)/), errors },
+		{ code: String(/(?<!(?=a))/), output: String(/(?!a)/), errors },
+		{ code: String(/(?<!(?!a))/), output: String(/(?=a)/), errors },
+		{ code: String(/(?<!(?<=a))/), output: String(/(?<!a)/), errors },
+		{ code: String(/(?<!(?<!a))/), output: String(/(?<=a)/), errors },
+	]
+});
