21da4f16c0b3e4c4ff7bdcaa8fbba33228d0f2bf
RunDevelopment <mitchi5000.ms@googlemail.com>
2019-09-03 00:07:06 +0200

    Improved no const capturing groups

--- tests/lib/rules/no-constant-capturing-groups.js
+++ tests/lib/rules/no-constant-capturing-groups.js
@@ -15,13 +15,9 @@ ruleTester.run("no-constant-capturing-groups", rule, {
 		String(/(a{2,3})/),
 	],
 	invalid: [
-		{
-			code: String(/()/),
-			errors: [{ message: "Empty capturing group" }]
-		},
-		{
-			code: String(/(\b(?:ab){3}$)/),
-			errors: [{ message: "Constant capturing group: The capturing group (\\b(?:ab){3}$) can only capture one word." }]
-		},
+		{ code: String(/()/), errors: [{ message: "Empty capturing group" }] },
+		{ code: String(/(\b)/), errors: [{ message: "The capturing group (\\b) can only capture the empty string." }] },
+		{ code: String(/(^\b|(?!b))/), errors: [{ message: "The capturing group (^\\b|(?!b)) can only capture the empty string." }] },
+		{ code: String(/(\b(?:ab){3}$)/), errors: [{ message: "The capturing group (\\b(?:ab){3}$) can only capture one word." }] },
 	]
 });
