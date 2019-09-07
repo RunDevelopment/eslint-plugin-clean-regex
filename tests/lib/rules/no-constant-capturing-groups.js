81ee8cf56a13a563b537552162a68470eb407d64
Michael Schmidt <mitchi5000.ms@googlemail.com>
2019-09-07 10:54:44 +0200

    NCCG: Added option to allow non-empty strings

--- tests/lib/rules/no-constant-capturing-groups.js
+++ tests/lib/rules/no-constant-capturing-groups.js
@@ -16,10 +16,13 @@ ruleTester.run("no-constant-capturing-groups", rule, {
 		String(/(a|b)/),
 		String(/(a)/i),
 		String(/(a|A)/),
+		{ code: String(/(a)/), options: [{ allowNonEmpty: true }] },
 	],
 	invalid: [
 		{ code: String(/()/), errors: [{ message: "Empty capturing group" }] },
+		{ code: String(/()/), options: [{ allowNonEmpty: true }], errors: [{ message: "Empty capturing group" }] },
 		{ code: String(/(\b)/), errors: [{ message: "The capturing group (\\b) can only capture the empty string." }] },
+		{ code: String(/(\b)/), options: [{ allowNonEmpty: true }], errors: [{ message: "The capturing group (\\b) can only capture the empty string." }] },
 		{ code: String(/(^\b|(?!b))/), errors: [{ message: "The capturing group (^\\b|(?!b)) can only capture the empty string." }] },
 		{ code: String(/(a|a|a)/), errors: [{ message: "The capturing group (a|a|a) can only capture one word which is \"a\"." }] },
 		{ code: String(/(,)/i), errors: [{ message: "The capturing group (,) can only capture one word which is \",\"." }] },
