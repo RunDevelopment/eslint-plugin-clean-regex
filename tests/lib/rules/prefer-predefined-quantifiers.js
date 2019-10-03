81cb8b5bb281cb2941ba1a6f1ff96412801d9cc2
RunDevelopment <mitchi5000.ms@googlemail.com>
2019-10-03 21:26:05 +0200

    Fixed PPQ

--- tests/lib/rules/prefer-predefined-quantifiers.js
+++ tests/lib/rules/prefer-predefined-quantifiers.js
@@ -7,9 +7,12 @@ const errors = [{ message: /^Use the predefined quantifier [+*?] instead\.$/ }];
 testRule(__filename, undefined, {
 	valid: [
 		String(/a*/),
+		String(/a?/),
+		String(/a+/),
 		String(/a{1}/),
 		String(/a{2,}/),
 		String(/a{0,2}/),
+		String(/catch\s+/),
 	],
 	invalid: [
 		{ code: String(/a{0,1}/), output: String(/a?/), errors },
