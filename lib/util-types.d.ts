325b4b0f4ed4a41044df39561464726c8a8383c7
Michael Schmidt <mitchi5000.ms@googlemail.com>
2020-03-14 19:41:14 +0100

    Util improvements

--- lib/util-types.d.ts
+++ lib/util-types.d.ts
@@ -1,6 +1,6 @@
 import { Node, Alternative, Character, Element, CharacterClass, CharacterClassRange, CharacterClassElement, RegExpLiteral, Flags, Pattern, Group, CapturingGroup, LookaroundAssertion, Quantifier, BranchNode } from "regexpp/ast";
 
-export declare type Descendants<T> = T extends Node ? T | RealDescendants<T> : never;
+export declare type Descendants<T> = T | (T extends Node ? RealDescendants<T> : never);
 
 type RealDescendants<T extends Node> =
 	T extends (Alternative | CapturingGroup | Group | LookaroundAssertion | Quantifier | Pattern) ? Element | CharacterClassElement : never |
