9d931f9fdc392e7b92e58c215403a33216dc6685
Michael Schmidt <mitchi5000.ms@googlemail.com>
2020-03-15 13:47:44 +0100

    OLQ: Fixed lookbehinds + doc

--- tests/lib/rules/optimal-lookaround-quantifier.js
+++ tests/lib/rules/optimal-lookaround-quantifier.js
@@ -2,18 +2,35 @@
 
 const { testRule } = require("../../test-util");
 
-const errors = [{ message: /^The quantified expression [\s\S]+ at the end of the expression tree should only be matched a constant number of times. The expression can be [\s\S]+ without affecting the lookaround.$/ }];
-
 testRule(__filename, undefined, {
 	valid: [
 		String(/(?=(a*))\w+\1/),
 		String(/(?<=a{4})/)
 	],
 	invalid: [
-		{ code: String(/(?=ba*)/), errors },
-		{ code: String(/(?=(?:a|b|abc*))/), errors },
-		{ code: String(/(?<=(?:a|(?:a|(?:abc*|))))/), errors },
-		{ code: String(/(?<=[a-c]*)/), errors },
-		{ code: String(/(?=ab(c)*)/), errors },
+		{
+			code: String(/(?=ba*)/),
+			errors: [{ message: "The quantified expression a* at the end of the expression tree should only be matched a constant number of times. The expression can be removed without affecting the lookaround." }]
+		},
+		{
+			code: String(/(?=(?:a|b|abc*))/),
+			errors: [{ message: "The quantified expression c* at the end of the expression tree should only be matched a constant number of times. The expression can be removed without affecting the lookaround." }]
+		},
+		{
+			code: String(/(?=(?:a|b|abc+))/),
+			errors: [{ message: "The quantified expression c+ at the end of the expression tree should only be matched a constant number of times. The expression can be replaced with c (no quantifier) without affecting the lookaround." }]
+		},
+		{
+			code: String(/(?=(?:a|b|abc{4,9}))/),
+			errors: [{ message: "The quantified expression c{4,9} at the end of the expression tree should only be matched a constant number of times. The expression can be replaced with c{4} without affecting the lookaround." }]
+		},
+		{
+			code: String(/(?<=[a-c]*)/),
+			errors: [{ message: "The quantified expression [a-c]* at the start of the expression tree should only be matched a constant number of times. The expression can be removed without affecting the lookaround." }]
+		},
+		{
+			code: String(/(?<=(c)*ab)/),
+			errors: [{ message: "The quantified expression (c)* at the start of the expression tree should only be matched a constant number of times. The expression can be removed without affecting the lookaround." }]
+		},
 	]
 });
