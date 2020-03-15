fa5bc2cf6310f230d92c4ff6922979c5881f907b
Michael Schmidt <mitchi5000.ms@googlemail.com>
2020-03-15 13:06:54 +0100

    NUCC: Implemented missing fetures

--- docs/rules/no-unnecessary-character-class.md
+++ docs/rules/no-unnecessary-character-class.md
@@ -9,4 +9,48 @@ Fixable: `yes` <br> Recommended configuration: `"warn"`
 
 ## Description
 
-TODO
+Unnecessary character classes contain only one character and can be trivially removed.
+E.g. `[a]`, `[\x61]`, `[\?]`.
+
+### `avoidEscape`
+
+Sometimes characters have to be escaped, in order to remove the character class (e.g. `a[+]` -> `a\+`). The automatic escaping can be disabled by using `{ avoidEscape: true }` in the rule configuration.
+
+Note that this option does not affect characters already escaped in the character class (e.g. `a[\+]` -> `a\+`).
+
+#### Without `avoidEscape: true`
+
+```
+/a[+]/ -> /a\+/
+/a[.]/ -> /a\./
+
+/a[\s]/ -> /a\s/
+/a[\+]/ -> /a\+/
+```
+
+#### With `avoidEscape: true`
+
+```
+/a[+]/ -> /a[+]/
+/a[.]/ -> /a[.]/
+
+/a[\s]/ -> /a\s/
+/a[\+]/ -> /a\+/
+```
+
+### `\b` (backspace)
+
+`\b` means backspace (`\x08`) inside of character classes but it will interpreted as a boundary assertion anywhere else. You can use `avoidEscape` to change how `[\b]` will handled.
+
+#### Without `avoidEscape: true`
+
+```
+/[\b]/ -> /\x08/
+```
+
+#### With `avoidEscape: true`
+
+```
+/[\b]/ -> /[\b]/
+```
+
--- tests/lib/rules/no-unnecessary-character-class.js
+++ tests/lib/rules/no-unnecessary-character-class.js
@@ -8,17 +8,18 @@ testRule(__filename, undefined, {
 	valid: [
 		String(/\c1/),
 		String(/[^]/),
-		String(/[$]/),
-		String(/[\b]/),
-		String(/[\B]/),
 		String(/[a-f]/),
 		String(/[^a]/),
+		String(/[^\n]/),
 		String(/{[2]}/),
 		String(/(.)\1[2]/),
-		{ code: String(/[.][*][+][?][{][}][(][)][[][\]][/][\^][$]/), options: [{ avoidEscape: true }] },
+		{ code: String(/[\b]/), options: [{ avoidEscape: true }] },
+		{ code: String(/[.][*][+][?][{][}][(][)][[][/][$]/), options: [{ avoidEscape: true }] },
+		String(/[\4]/), // octal escape
+		String(/(?<foo>a*)[\k]<foo>/),
 	],
 	invalid: [
-		//{ code: String(/[a]/), errors: [{ message: "" }] },
+		// character sets
 		{ code: String(/[\w]/), output: String(/\w/), errors },
 		{ code: String(/[\W]/), output: String(/\W/), errors },
 		{ code: String(/[\s]/), output: String(/\s/), errors },
@@ -28,5 +29,52 @@ testRule(__filename, undefined, {
 		{ code: String(/[^\s]/), output: String(/\S/), errors },
 		{ code: String(/[^\S]/), output: String(/\s/), errors },
 		{ code: String(/[^\p{Script_Extensions=Greek}]/u), output: String(/\P{Script_Extensions=Greek}/u), errors },
+
+		// special characters
+		{ code: String(/[.]/), output: String(/\./), errors },
+		{ code: String(/[*]/), output: String(/\*/), errors },
+		{ code: String(/[+]/), output: String(/\+/), errors },
+		{ code: String(/[?]/), output: String(/\?/), errors },
+		{ code: String(/[{]/), output: String(/\{/), errors },
+		{ code: String(/[}]/), output: String(/\}/), errors },
+		{ code: String(/[(]/), output: String(/\(/), errors },
+		{ code: String(/[)]/), output: String(/\)/), errors },
+		{ code: String(/[[]/), output: String(/\[/), errors },
+		{ code: String(/[/]/), output: String(/\//), errors },
+		{ code: String(/[$]/), output: String(/\$/), errors },
+
+		// backspace
+		{ code: String(/[\b]/), output: String(/\x08/), errors },
+
+		// escape sequences
+		{ code: String(/[\0]/), output: String(/\0/), errors },
+		{ code: String(/[\x02]/), output: String(/\x02/), errors },
+		{ code: String(/[\uFFFF]/), output: String(/\uFFFF/), errors },
+		{ code: String(/[\u{10FFFF}]/u), output: String(/\u{10FFFF}/u), errors },
+		{ code: String(/[\cI]/), output: String(/\cI/), errors },
+		{ code: String(/[\f]/), output: String(/\f/), errors },
+		{ code: String(/[\n]/), output: String(/\n/), errors },
+		{ code: String(/[\r]/), output: String(/\r/), errors },
+		{ code: String(/[\t]/), output: String(/\t/), errors },
+		{ code: String(/[\v]/), output: String(/\v/), errors },
+
+		// literals
+		{ code: String(/[a]/), output: String(/a/), errors },
+		{ code: String(/[a]/i), output: String(/a/i), errors },
+		{ code: String(/[H]/), output: String(/H/), errors },
+		{ code: String(/[%]/), output: String(/%/), errors },
+
+		// escaped literals
+		{ code: String(/[\a]/), output: String(/\a/), errors },
+		{ code: String(/[\g]/), output: String(/\g/), errors },
+		{ code: String(/[\H]/), output: String(/\H/), errors },
+		{ code: String(/[\"]/), output: String(/\"/), errors },
+		{ code: String(/[\\]/), output: String(/\\/), errors },
+		{ code: String(/[\-]/), output: String(/\-/), errors },
+		{ code: String(/[\]]/), output: String(/\]/), errors },
+		{ code: String(/[\^]/), output: String(/\^/), errors },
+		{ code: String(/[\/]/), output: String(/\//), errors },
+		{ code: String(/[\%]/), output: String(/\%/), errors },
+
 	]
 });
