59af8cedee595010ed1211a8e365e20716a28ff8
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-04-01 18:08:03 +0200

    NCCG: Less annoying default

--- docs/rules/no-constant-capturing-group.md
+++ docs/rules/no-constant-capturing-group.md
@@ -12,5 +12,39 @@ Fixable: `no` <br> Recommended configuration: `"warn"`
 Constant capturing groups can only match one word.
 
 Because they can only match one word, they should be replaced with the constant string they capture for better performance.
+This is especially the case if the capturing groups only matches the empty word.
 
-E.g. `/(foo)/`
+E.g. `/(foo)/`, `a()b`, `a(\b)`
+
+### `ignoreNonEmpty: boolean`
+
+If this option is get to `true`, the rule will ignore capturing groups that can match non-empty words.
+This option is `true` by default.
+
+#### `ignoreNonEmpty: true`
+
+```js
+// valid
+/(a)/i
+/(a|b)/
+/(a*)/
+/(a)/  // constant but it doesn't match the empty word
+
+// invalid
+/()/   // warn about `()`
+/(\b)/ // warn about `(\b)`
+```
+
+#### `ignoreNonEmpty: false`
+
+```js
+// valid
+/(a)/i
+/(a|b)/
+/(a*)/
+
+// invalid
+/(a)/  // warn about `(a)`
+/()/   // warn about `()`
+/(\b)/ // warn about `(\b)`
+```
--- tests/lib/rules/no-constant-capturing-group.js
+++ tests/lib/rules/no-constant-capturing-group.js
@@ -9,16 +9,23 @@ testRule(__filename, undefined, {
 		String(/(a|b)/),
 		String(/(a)/i),
 		String(/(a|A)/),
-		{ code: String(/(a)/), options: [{ allowNonEmpty: true }] },
+		String(/(a)/),
+		{ code: String(/(a)/), options: [{ ignoreNonEmpty: true }] },
 	],
 	invalid: [
 		{ code: String(/()/), errors: [{ message: "Empty capturing group" }] },
-		{ code: String(/()/), options: [{ allowNonEmpty: true }], errors: [{ message: "Empty capturing group" }] },
+		{ code: String(/()/), options: [{ ignoreNonEmpty: false }], errors: [{ message: "Empty capturing group" }] },
+		{ code: String(/()/), options: [{ ignoreNonEmpty: true }], errors: [{ message: "Empty capturing group" }] },
+
 		{ code: String(/(\b)/), errors: [{ message: "The capturing group (\\b) can only capture the empty string." }] },
-		{ code: String(/(\b)/), options: [{ allowNonEmpty: true }], errors: [{ message: "The capturing group (\\b) can only capture the empty string." }] },
+		{ code: String(/(\b)/), options: [{ ignoreNonEmpty: false }], errors: [{ message: "The capturing group (\\b) can only capture the empty string." }] },
+		{ code: String(/(\b)/), options: [{ ignoreNonEmpty: true }], errors: [{ message: "The capturing group (\\b) can only capture the empty string." }] },
+
 		{ code: String(/(^\b|(?!b))/), errors: [{ message: "The capturing group (^\\b|(?!b)) can only capture the empty string." }] },
-		{ code: String(/(a|a|a)/), errors: [{ message: "The capturing group (a|a|a) can only capture one word which is \"a\"." }] },
-		{ code: String(/(,)/i), errors: [{ message: "The capturing group (,) can only capture one word which is \",\"." }] },
-		{ code: String(/(\b(?:ab){3}$)/), errors: [{ message: "The capturing group (\\b(?:ab){3}$) can only capture one word which is \"ababab\"." }] },
+
+		{ code: String(/(a|a|a)/), options: [{ ignoreNonEmpty: false }], errors: [{ message: "The capturing group (a|a|a) can only capture one word which is \"a\"." }] },
+
+		{ code: String(/(,)/i), options: [{ ignoreNonEmpty: false }], errors: [{ message: "The capturing group (,) can only capture one word which is \",\"." }] },
+		{ code: String(/(\b(?:ab){3}$)/), options: [{ ignoreNonEmpty: false }], errors: [{ message: "The capturing group (\\b(?:ab){3}$) can only capture one word which is \"ababab\"." }] },
 	]
 });
