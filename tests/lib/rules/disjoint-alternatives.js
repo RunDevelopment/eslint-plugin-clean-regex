e8afe439e191c4e541656f7249bec8e90f8b12f6
Michael Schmidt <mitchi5000.ms@googlemail.com>
2020-08-24 22:37:35 +0200

    New rule: Disjoint alternatives (#22)

---
+++ tests/lib/rules/disjoint-alternatives.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const { testRule } = require("../../test-util");
+
+testRule(__filename, undefined, {
+	valid: [
+		String(/a+|b+/),
+		String(/a?|a{2,}/),
+		String(/a*|b*/),
+	],
+	invalid: [
+		{
+			code: String(/b+(?:\w+|[+-]?\d+)/),
+			errors: [{ message: "This alternative is not disjoint with `\\w+`. The shared language is /\\d+/i." }]
+		},
+		{
+			code: String(/FOO|foo(?:bar)?/i),
+			errors: [{ message: "This alternative is a superset of `FOO`." }]
+		},
+		{
+			code: String(/foo(?:bar)?|foo/),
+			errors: [{ message: "This alternative is a subset of `foo(?:bar)?` and can be removed." }]
+		},
+		{
+			code: String(/(?=[\t ]+[\S]{1,}|[\t ]+['"][\S]|[\t ]+$|$)/),
+			errors: [{ message: "This alternative is a subset of `[\\t ]+[\\S]{1,}` and can be removed." }]
+		},
+		{
+			code: String(/\w+(?:\s+(?:\S+|"[^"]*"))*/),
+			errors: [{ message: "This alternative is not disjoint with `\\S+`. The shared language is /\"[^\\s\"]*\"/i. This alternative is likely to cause exponential backtracking." }]
+		},
+	]
+});
