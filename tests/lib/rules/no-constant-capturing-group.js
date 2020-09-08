fb0ca014b9adff60686c83aa19b238ba26e0796c
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-09-08 11:03:31 +0200

    More consistent mentions in messages

--- tests/lib/rules/no-constant-capturing-group.js
+++ tests/lib/rules/no-constant-capturing-group.js
@@ -17,15 +17,15 @@ testRule(__filename, undefined, {
 		{ code: String(/()/), options: [{ ignoreNonEmpty: false }], errors: [{ message: "Empty capturing group" }] },
 		{ code: String(/()/), options: [{ ignoreNonEmpty: true }], errors: [{ message: "Empty capturing group" }] },
 
-		{ code: String(/(\b)/), errors: [{ message: "The capturing group (\\b) can only capture the empty string." }] },
-		{ code: String(/(\b)/), options: [{ ignoreNonEmpty: false }], errors: [{ message: "The capturing group (\\b) can only capture the empty string." }] },
-		{ code: String(/(\b)/), options: [{ ignoreNonEmpty: true }], errors: [{ message: "The capturing group (\\b) can only capture the empty string." }] },
+		{ code: String(/(\b)/), errors: [{ message: "The capturing group `(\\b)` can only capture the empty string." }] },
+		{ code: String(/(\b)/), options: [{ ignoreNonEmpty: false }], errors: [{ message: "The capturing group `(\\b)` can only capture the empty string." }] },
+		{ code: String(/(\b)/), options: [{ ignoreNonEmpty: true }], errors: [{ message: "The capturing group `(\\b)` can only capture the empty string." }] },
 
-		{ code: String(/(^\b|(?!b))/), errors: [{ message: "The capturing group (^\\b|(?!b)) can only capture the empty string." }] },
+		{ code: String(/(^\b|(?!b))/), errors: [{ message: "The capturing group `(^\\b|(?!b))` can only capture the empty string." }] },
 
-		{ code: String(/(a|a|a)/), options: [{ ignoreNonEmpty: false }], errors: [{ message: "The capturing group (a|a|a) can only capture one word which is \"a\"." }] },
+		{ code: String(/(a|a|a)/), options: [{ ignoreNonEmpty: false }], errors: [{ message: "The capturing group `(a|a|a)` can only capture one word which is \"a\"." }] },
 
-		{ code: String(/(,)/i), options: [{ ignoreNonEmpty: false }], errors: [{ message: "The capturing group (,) can only capture one word which is \",\"." }] },
-		{ code: String(/(\b(?:ab){3}$)/), options: [{ ignoreNonEmpty: false }], errors: [{ message: "The capturing group (\\b(?:ab){3}$) can only capture one word which is \"ababab\"." }] },
+		{ code: String(/(,)/i), options: [{ ignoreNonEmpty: false }], errors: [{ message: "The capturing group `(,)` can only capture one word which is \",\"." }] },
+		{ code: String(/(\b(?:ab){3}$)/), options: [{ ignoreNonEmpty: false }], errors: [{ message: "The capturing group `(\\b(?:ab){3}$)` can only capture one word which is \"ababab\"." }] },
 	]
 });
