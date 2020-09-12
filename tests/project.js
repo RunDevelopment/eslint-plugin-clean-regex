d47de2db4a216cefa1ae3dea8420d8a0e2f0023d
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-09-12 23:19:20 +0200

    Tests: Make preject test more resistent against temporary files

--- tests/project.js
+++ tests/project.js
@@ -1,10 +1,18 @@
 const fs = require("fs");
 const { assert } = require("chai");
 
+/**
+ * @param {string} path
+ * @returns {boolean}
+ */
+function relevantFile(path) {
+	return /\.(?:js|md|json)$/.test(path);
+}
+
 describe("Project", () => {
-	const rules = fs.readdirSync(`${__dirname}/../lib/rules`);
-	const ruleTests = fs.readdirSync(`${__dirname}/../tests/lib/rules`);
-	const ruleDocs = fs.readdirSync(`${__dirname}/../docs/rules`);
+	const rules = fs.readdirSync(`${__dirname}/../lib/rules`).filter(relevantFile);
+	const ruleTests = fs.readdirSync(`${__dirname}/../tests/lib/rules`).filter(relevantFile);
+	const ruleDocs = fs.readdirSync(`${__dirname}/../docs/rules`).filter(relevantFile);
 
 	it("should have test files for every rule", () => {
 		assert.sameMembers(rules, ruleTests);
